b7ce6984522e319b7bcba5f44179e5788d50c7f1
bipinbachhao <bipinbachhao@gmail.com>
2020-05-02 19:56:53 -0400

    Chef-16 and centos-8 support added

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
-default['multi_python']['version']['2'] = '2.7.15'
-default['multi_python']['version']['3'] = '3.7.0'
+default['multi_python']['version']['2'] = '2.7.18'
+default['multi_python']['version']['3'] = '3.8.2'
 
 node['multi_python']['version'].each do |version, release|
   default['multi_python']['download_url'][version] = "https://www.python.org/ftp/python/#{release}/Python-#{release}.tar.xz"
--- recipes/prerequisites.rb
+++ recipes/prerequisites.rb
@@ -7,8 +7,9 @@
 include_recipe 'build-essential'
 
 case node['platform_family']
-  when 'debian'
-    package %w[build-essential libsasl2-dev openssl libssl-dev libnss3-dev xz-utils libsqlite3-dev libbz2-dev libgdbm-dev tcl-dev tk-dev tcl tk libncursesw5-dev libreadline-gplv2-dev libc6-dev libffi-dev]
-  when 'amazon', 'fedora', 'rhel'
-    package %w[openssl openssl-devel zlib-devel bzip2 bzip2-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel xz xz-libs xz-devel libffi-devel sqlite-devel libpcap-devel expat-devel]
+when 'debian'
+  package %w(build-essential libsasl2-dev openssl libssl-dev libnss3-dev xz-utils libsqlite3-dev libbz2-dev libgdbm-dev tcl-dev tk-dev tcl tk libncursesw5-dev libreadline-gplv2-dev libc6-dev libffi-dev)
+when 'amazon', 'fedora', 'rhel'
+  package %w(libpcap-devel) unless node['platform_version'] > '8'
+  package %w(openssl openssl-devel zlib-devel bzip2 bzip2-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel xz xz-libs xz-devel libffi-devel sqlite-devel expat-devel)
 end
